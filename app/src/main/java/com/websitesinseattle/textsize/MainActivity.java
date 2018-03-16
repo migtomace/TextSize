2d934897d9e73a530805832acec06420fcd74335
migtomace <32564100+migtomace@users.noreply.github.com>
2018-03-16 02:40:27 -0700

    This updates the app with onPause, onResume, and preferences

--- app/src/main/java/com/websitesinseattle/textsize/MainActivity.java
+++ app/src/main/java/com/websitesinseattle/textsize/MainActivity.java
@@ -1,20 +1,44 @@
 package com.websitesinseattle.textsize;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+import static android.content.ContentValues.TAG;
+
+public class MainActivity extends Activity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener, TextView.OnEditorActionListener {
 
     private EditText editText;
     private Button button;
     private SeekBar seekBar;
     private TextView textView;
+
+    //define the shared preferences object
+    private SharedPreferences savedValues;
+
+    //string
+    private String fontString = "";
+
+    //define instant variables
+    private final int NONE = 0;
+    private final int HALF = 1;
+
+    private SharedPreferences prefs;
+    private boolean rememberFontSizePrefs = true;
+    private int fontChange = NONE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +53,89 @@ public class MainActivity extends Activity implements View.OnClickListener, Seek
 
         //create listener
         button.setOnClickListener(this);
+        editText.setOnEditorActionListener(this);
         seekBar.setOnSeekBarChangeListener(this);
 
+        // get SharedPreferences object
+        savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
+
+        //set the default values for the prefs
+        PreferenceManager.setDefaultValues(this,R.xml.preferences, false);
+
+        //get the default shared prefs object
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+
+
+    }
+
+    @Override
+    public void onPause() {
+        // save the instance variables
+        SharedPreferences.Editor editor = savedValues.edit();
+        editor.putString("fontString", fontString);
+        editor.commit();
+
+        super.onPause();
+
+        Log.d(TAG, "onPause executed");
+
+        //create a toast
+        Toast t = Toast.makeText(this, "onPause method", Toast.LENGTH_LONG);
+        t.show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // get preferences
+        rememberFontSizePrefs = prefs.getBoolean("pref_remember_input", true);
+        fontChange = Integer.parseInt(prefs.getString("pref_fontSize", "0"));
+
+        // get the instance variables
+        fontString = savedValues.getString("fontString", "");
+
+        // set the saved text input on its widget
+        editText.setText(fontString);
+
+        // display the saved text
+        displayText();
+
+        Log.d(TAG, "onResume executed");
+
+        //create a toast
+        Toast t = Toast.makeText(this, "onResume method", Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.menu:
+                startActivity(new Intent(getApplicationContext(),
+                        SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+            displayText();
+        }
+        return false;
     }
 
     @Override
@@ -40,28 +145,47 @@ public class MainActivity extends Activity implements View.OnClickListener, Seek
 
     public void displayText(){
         String text = editText.getText().toString();
+
+
         textView.setText(text);
+
+
+
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
         Toast.makeText(getApplicationContext(), "seekbar started: ", Toast.LENGTH_SHORT).show();
-        textView.setTextSize(i);
+
+
+        fontString = editText.getText().toString();
+
+        int divide = i/2;
+
+        if(fontChange == NONE){
+            textView.setTextSize(i);
+            Toast.makeText(getApplicationContext(), "no pref", Toast.LENGTH_SHORT).show();
+        } else if (fontChange == HALF) {
+            textView.setTextSize(divide);
+            Toast.makeText(getApplicationContext(),"divide",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(),"error", Toast.LENGTH_LONG).show();
+        }
+
     }
 
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
-        Toast. makeText(getApplicationContext(), "seekbar started: ", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "seekbar started: ", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
         int progress = seekBar.getProgress();
 
-        Toast. makeText(getApplicationContext(), "seekbar touch stopped ", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "seekbar touch stopped ", Toast.LENGTH_SHORT).show();
         if(progress >= 10){
             Toast.makeText(getApplicationContext(), "Expert Level", Toast.LENGTH_SHORT).show();
         }
     }
 }
-
